44573fcd769f42c59d7a86f453878279469dd1db
Claire Cashmore <clairecashmore2017@gmail.com>
2022-10-04 08:30:39 -0400

    manually add build

---
+++ client/build/precache-manifest.228052c725cc85efcb92bec324e5632d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be34c653b1839503e96591a699f9556b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f08d0c6c75ea3156c66",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "41e3045a55716a1949c1",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "9f08d0c6c75ea3156c66",
+    "url": "/static/js/2.c6379c33.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c6379c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41e3045a55716a1949c1",
+    "url": "/static/js/main.3ce551a1.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
